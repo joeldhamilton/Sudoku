530cb971b38988fd9f6e1ed30ff648e9dc59a5de
joeldhamilton <joeldhamilton@gmail.com>
2025-08-24 12:48:13 -0400

    Initial Commit

---
+++ Sudoku/Sudoku/SudokuApp.swift
@@ -0,0 +1,17 @@
+//
+//  SudokuApp.swift
+//  Sudoku
+//
+//  Created by Joel Hamilton on 8/24/25.
+//
+
+import SwiftUI
+
+@main
+struct SudokuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
